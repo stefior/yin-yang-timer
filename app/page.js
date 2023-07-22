d0a45b41b4626850bbc1059c1a3dd2d0b32eef7a
stefior <stephencfiorenza@gmail.com>
2023-07-22 06:58:33 -0400

    feat: implement basic stopwatch/countdown timer

--- app/page.js
+++ app/page.js
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+"use client";
+
+import { useState, useEffect } from "react";
+import useTimer from "easytimer-react-hook";
 
 export default function Home() {
+  const [stopwatch, isStopwatchAchieved] = useTimer();
+  const [updateWhenTargetAchieved, setUpdateWhenTargetAchieved] =
+  useState(true);
+  const [countdown, isCountdownAchieved] = useTimer({
+    updateWhenTargetAchieved,
+  });
+  const [yinYang, setYinYang] = useState(0);
+
+  stopwatch.start({});
+
+  useEffect(() => {
+    document.body.style.backgroundColor = yinYang ? "black" : "white";
+    console.log(isCountdownAchieved);
+    if (yinYang) {
+      stopwatch.pause();
+      countdown.start({
+        startValues: { ...stopwatch.getTimeValues() },
+        countdown: true,
+        target: { seconds: 0 },
+      });
+      stopwatch.stop();
+    } else {
+      countdown.reset();
+      countdown.stop();
+      stopwatch.reset();
+    }
+  }, [yinYang]);
+
+  useEffect(() => {
+    setYinYang(0);
+  }, [isCountdownAchieved]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div
+        className={`text-[12vw] ${
+          yinYang ? "text-white" : "text-black font-light"
+        } transition-all`}
+      >
+        {/* TOTO: FIX JITTER */}
+        {yinYang
+          ? countdown.getTimeValues().toString()
+          : stopwatch.getTimeValues().toString()}
+      </div>
+      <button
+        className={`overflow-hidden w-[8vw] h-[8vw] mt-8 rounded-full px-1 py-1 ${
+          yinYang ? "" : "ring-black hover:ring-4"
+        } transition-all`}
+        onClick={() => {
+          yinYang ? setYinYang(0) : setYinYang(1);
+        }}
+        disabled={yinYang ? true : false}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          id="svg2"
+          viewBox="0 0 720 720"
+          version="1.0"
+        >
+          <g id="layer1">
+            <path
+              id="path2413"
+              d="m360 0v720c198.72 0 360-161.28 360-360s-161.28-360-360-360z"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <path
+              id="path2466"
+              fill="#fff"
+              d="m360 720v-720c-198.72 0-360 161.28-360 360s161.28 360 360 360z"
+            />
+            <path
+              id="path2468"
+              fill="#fff"
+              transform="matrix(4,0,0,4,540,-720)"
+              d="m0 225a45 45 0 1 1 -90 0 45 45 0 1 1 90 0z"
+            />
+            <path
+              id="path2470"
+              transform="matrix(4,0,0,4,540,-360)"
+              d="m0 225a45 45 0 1 1 -90 0 45 45 0 1 1 90 0z"
+            />
+            <path
+              id="path2472"
+              transform="translate(405,-45)"
+              d="m0 225a45 45 0 1 1 -90 0 45 45 0 1 1 90 0z"
+            />
+            <path
+              id="path2474"
+              fill="#fff"
+              transform="translate(405,315)"
+              d="m0 225a45 45 0 1 1 -90 0 45 45 0 1 1 90 0z"
+            />
+          </g>
+        </svg>
+      </button>
+    </>
+  );
 }
